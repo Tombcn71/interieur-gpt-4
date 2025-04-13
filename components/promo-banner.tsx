79e25946add4d80bf7746d6de9ab2abdf87c0bfe
Tom <tbvanreijn@gmail.com>
2025-04-13 21:14:02 +0200

    code

--- components/promo-banner.tsx
+++ components/promo-banner.tsx
@@ -3,12 +3,12 @@ export function PromoBanner() {
     <div className="bg-blue-500 text-center py-3 px-4 text-white">
       <p className="text-xs sm:text-sm md:text-base font-medium">
         <span className="font-bold">PAASDAGEN AANBIEDING!</span>{" "}
-        <span className="hidden xs:inline">Gebruik code </span>
+        <span className="">Gebruik code </span>
         <span className="bg-yellow-500 text-black px-2 py-0.5 rounded font-bold mx-1">
-          "PASEN"
+          "BF60"
         </span>{" "}
-        <span className="hidden xs:inline">bij het afrekenen voor </span>
-        <span className="font-bold">60%</span> korting!
+        <span className=""> voor </span>
+        <span className="font-bold">50%</span> korting!
       </p>
     </div>
   );
